d5fd0003430bb17fca82ee106284c8b73c77b2d2
lm.mo <jasminemei@163.com>
2020-12-04 15:24:32 +0800

    添加vue.config.js配置

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+
+const path = require('path')
+const resolve = function (dir) {
+  return path.join(__dirname, dir)
+}
+// vue.config.js
+module.exports = {
+  publicPath: '/',
+  outputDir: process.env.VUE_APP_PACKAGE_NAME || 'dist',
+  assetsDir: 'static',
+  lintOnSave: process.env.VUE_APP_NODE_ENV === 'development',
+  productionSourceMap: false,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('@', resolve('src/views'))
+      .set('@', resolve('src/components'))
+      .set('@', resolve('src/common'))
+      .set('@', resolve('src/utils'))
+      .set('@', resolve('src/service')) /* 别名配置 */
+    config.optimization.runtimeChunk('single')
+  }
+}
